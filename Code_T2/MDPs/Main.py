1b05d0b956d3aec219176dded6760da3a5714cb7
Diego-Galleguillos <dpgalleguillos@uc.cl>
2025-08-30 11:56:44 -0400

    policy_iter_grid

--- Code_T2/MDPs/Main.py
+++ Code_T2/MDPs/Main.py
@@ -1,4 +1,4 @@
-import random
+import random, numpy
 
 from Problems.CookieProblem import CookieProblem
 from Problems.GridProblem import GridProblem
@@ -34,10 +34,43 @@ def play(problem):
         transitions = problem.get_transitions(state, action)
         s_next, reward = sample_transition(transitions)
         done = problem.is_terminal(s_next)
+        print(state)
         state = s_next
+        print(state)
         total_reward += reward
     print("Done.")
     print(f"Total reward: {total_reward}")
+
+def iterative_pol_grid(size, discount):
+    problem = GridProblem(size)
+    V = numpy.zeros(size*size)
+    state = problem.get_initial_state()
+    theta = 1
+    counter = 0
+    while theta > 0.0000000001:
+        theta = 0
+        counter += 1
+        for i in range(size*size):
+            skip = problem.is_terminal(i)
+            if not skip:
+                state = i
+                v = V[i]
+                actions = problem.get_available_actions(state)
+                sum_actions = 0
+                for action in actions:
+                    transitions = problem.get_transitions(state, action)
+                    sum_dynamics = 0
+                    for prob, s_next, reward in transitions:
+                        sum_dynamics += prob*(reward+discount*V[s_next])
+                    sum_actions += (1/(len(actions)))*sum_dynamics
+                V[i] = sum_actions
+                theta = max(theta, abs(V[i]-v))
+
+    value_matrix = numpy.reshape(V, (size, size))
+    print("Value Function Matrix:")
+    for row in value_matrix:
+        print(row)
+    print(counter)
 
 
 def play_gambler_problem():
@@ -59,6 +92,7 @@ def play_cookie_problem():
 
 
 if __name__ == '__main__':
-    # play_grid_problem()
+    #play_grid_problem()
     play_cookie_problem()
-    # play_gambler_problem()
+    #play_gambler_problem()
+    #iterative_pol_grid(10, 1)
