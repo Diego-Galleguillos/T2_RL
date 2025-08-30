70d2f46c0df8603e6ab0207a8a8cedd083db851e
Diego-Galleguillos <dpgalleguillos@uc.cl>
2025-08-30 16:17:52 -0400

    code 1

--- Code_T2/MDPs/Main.py
+++ Code_T2/MDPs/Main.py
@@ -35,6 +35,7 @@ def play(problem):
         s_next, reward = sample_transition(transitions)
         done = problem.is_terminal(s_next)
         print(state)
+        print(type(state))
         state = s_next
         print(state)
         total_reward += reward
@@ -72,6 +73,61 @@ def iterative_pol_grid(size, discount):
         print(row)
     print(counter)
 
+def iterative_pol_cookie(size, discount):
+    problem = CookieProblem(size)
+    V = numpy.zeros((size*size,size*size))
+    theta = 1
+    counter = 0
+    while theta > 0.0000000001:
+        theta = 0
+        counter += 1
+        for i in range(size*size):
+            for j in range(size*size):
+                skip = False
+                if not skip:
+                    state = (i,j)
+                    v = V[i][j]
+                    actions = problem.get_available_actions(state)
+                    sum_actions = 0
+                    for action in actions:
+                        transitions = problem.get_transitions(state, action)
+                        sum_dynamics = 0
+                        for prob, s_next, reward in transitions:
+                            sum_dynamics += prob*(reward+discount*V[s_next[0]][s_next[1]])
+                        sum_actions += (1/(len(actions)))*sum_dynamics
+                    V[i][j] = sum_actions
+                    theta = max(theta, abs(V[i][j]-v))
+
+
+def iterative_pol_gamble(p, discount):
+    problem = GamblerProblem(p)
+    V = numpy.zeros((101))
+    theta = 1
+    counter = 0
+    while theta > 0.0000000001:
+        theta = 0
+        counter += 1
+        for i in range(100):
+            skip = i == 100 or i == 0
+            if not skip:
+                state = i
+                v = V[i]
+                actions = problem.get_available_actions(state)
+                sum_actions = 0
+                for action in actions:
+                    transitions = problem.get_transitions(state, action)
+                    sum_dynamics = 0
+                    for prob, s_next, reward in transitions:
+                        sum_dynamics += prob*(reward+discount*V[s_next])
+                    sum_actions += (1/(len(actions)))*sum_dynamics
+                V[i] = sum_actions
+                theta = max(theta, abs(V[i]-v))
+
+
+    print("Value Function Matrix:")
+    print(V)
+    print(counter)
+
 
 def play_gambler_problem():
     p = 0.4
@@ -93,6 +149,6 @@ def play_cookie_problem():
 
 if __name__ == '__main__':
     #play_grid_problem()
-    play_cookie_problem()
+    #play_cookie_problem()
     #play_gambler_problem()
-    #iterative_pol_grid(10, 1)
+    iterative_pol_gamble(0.4, 1)
